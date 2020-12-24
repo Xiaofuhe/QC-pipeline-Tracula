ca8ffaabc878dd34fa4daa52caef2201d65dd4b8
Xiaofuhe <xiaofuhe2008@yahoo.com>
2020-12-23 20:26:41 -0500

    Update showFAandColorMap_batch.m

--- src/step2/showFAandColorMap/showFAandColorMap_batch.m
+++ src/step2/showFAandColorMap/showFAandColorMap_batch.m
@@ -1,9 +1,7 @@
 function showFAandColorMap_batch()
-%based on showFAandColorMap_GUI.m but automatically generate CFA and FA maps  that RAs can check the qc of CFA manually and visually
+%based on showFAandColorMap_GUI.m but automatically generate CFA image so that RAs can check the qc of CFA manually and visually
 
 % show FA and colormap in a batch
-% if you want to show CFA image only, please use showFAColorMap_basedonCMP_batch.m which is based on .cmp file, faster than this function since it need to generate CFA,FA,MD..from .d file
-% similar with showFAandColorMap_GUI which is only show one data
 % help:
 %.....use downarrow  (slice#-1) and uparrow (slice#+1) in the direction keys to control slice number
 %.....use 'Escape' key to exit
@@ -33,8 +31,7 @@ tempCD=cd;
 examParameter.maskPath=[];%default no mask path, will auto load it below
 showPara.savedImagePath=savedpath;
 showPara.ifAutoSave=1;%autosave some maps
-showPara.ifSaveCFA=1;%save CFA only which will speed up! but will only work for '.d' file instead of '.mat' which DAIdata was already generated
-                     %so, you may regenerate '.mat' by set ifSaveDAI=1
+showPara.ifSaveCFA=1;%save CFA only which will speed up!
 
 examParameter.tensorSuffix='_eddy_tensor.nii.gz';%e.g., /sub-01/FDT/sub-01_dwi_eddy_tensor.nii.gz
 examParameter.maskSuffix='_eddy_b0_noskull.nii.gz';%e.g., /sub-01/FDT/sub-01_dwi_eddy_b0_noskull.nii.gz
